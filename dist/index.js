0ad760b8bcb19d6f089108c1843fd38c20497c67
Pablo Gonzalez <pablo.regresion@gmail.com>
2015-10-13 16:42:13 +0200

    button theme ij WIP

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,1 @@
-'use strict';
-
-Object.defineProperty(exports, '__esModule', {
-  value: true
-});
-exports.button = button;
-
-function button() {
-  console.log('hello world');
-}
-
-;
+"use strict";
